399681182834d57e9d844940f147f19365668609
Nickan <Nickan.bagherpour@gmail.com>
2022-03-25 16:03:50 +0430

    feat: add simple createGlobalStyle

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import { Button } from "components/common";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+	body{
+		background: white;
+		min-height: 100vh;
+		margin: 0;
+		color: black;
+		font-family: 'Kaushan Script'
+	}
+`;
 
 const App = () => {
   return (
     <>
+      <GlobalStyle />
       <h1>Hello</h1>
-      <Button disabled={false}>Test</Button>
+      <Button>Test</Button>
     </>
   );
 };
